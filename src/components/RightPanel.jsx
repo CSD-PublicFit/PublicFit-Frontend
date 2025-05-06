d78535c2ed6d10b9576e77c8be306dbf7f6dfd0c
Kihwan Shin <ha2shin2@naver.com>
2025-05-06 22:36:54 +0900

    [feat] pdf viewer design
    pdf viewer design 수정

--- src/components/RightPanel.jsx
+++ src/components/RightPanel.jsx
@@ -12,26 +12,44 @@ export default function RightPanel({ loading, onGenerate, result }) {
   const handleLoadSuccess = ({numPages}) => {
     setNumPages(numPages);
   };
+
+  const handleDownload = () => {
+
+  };
   
   return (
     <div className="right-panel">
       <p className="note">📄 본 레포트는 ChatGPT의 도움을 받아 작성됩니다.</p>
-      <button className="report-button" onClick={onGenerate} disabled={loading}>
-        레포트 작성하기
+      <button className="report-button" onClick={onGenerate} disabled={loading || result} 
+      style={{
+          backgroundColor: result ? "#4ade80" : "#E6EAF5",
+          color: result ? "white" : "#5B7EF0"}}
+          >
+        {result ? "레포트 작성완료" : "레포트 작성하기"}
       </button>
       {loading && (
         <div className="loading-box">
-          작성중입니다 ...
+          작성중입니다...
         </div>
       )}
 
       {result && (   
-        <div className="pdf-viewer">
-          <Document file={result} onLoadSuccess={handleLoadSuccess}>
-            {Array.from(new Array(numPages), (_, index) => (
-              <Page key={index + 1} pageNumber={index + 1} />
-            ))}
-          </Document>
+        <div className="pdf-viewer-container">
+          <div className="generate-complete-message">
+            작성이 완료되었습니다!
+          </div>
+          <div className="pdf-viewer">
+            <Document file={result} onLoadSuccess={handleLoadSuccess}>
+              {Array.from(new Array(numPages), (_, index) => (
+                <Page key={index + 1} pageNumber={index + 1} />
+              ))}
+            </Document>
+          </div>
+          <div className="pdf-save-container">
+              <button className="pdf-save-button" onClick={handleDownload}>
+                pdf로 저장
+          </button>
+          </div>
         </div>
       )}
     </div>
